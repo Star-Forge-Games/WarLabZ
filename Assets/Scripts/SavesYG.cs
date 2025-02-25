845b7a6704087ad0e55a06439ed88d841c044932
Ivan <carefall.regular@gmail.com>
2025-02-25 19:02:36 +0300

    SaveData

---
+++ Assets/Scripts/SavesYG.cs
@@ -0,0 +1,17 @@
+namespace YG
+{
+    public partial class SavesYG
+    {
+        public int[] levelStars;
+        public int cash;
+        public int donateCash;
+        public int[] unlockedWeapons;
+        public int[] supplies;
+        public int points;
+        public int level;
+        public int[] achievements;
+        public int keys;
+        public int wallLevel;
+        public int[,] upgrades;
+    }
+}
