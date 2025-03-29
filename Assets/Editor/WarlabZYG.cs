e1dd559d8db677e381b086ec22c73f7cc70307d1
Ivan <carefall.regular@gmail.com>
2025-03-29 17:29:22 +0300

    Editor Thing

---
+++ Assets/Editor/WarlabZYG.cs
@@ -0,0 +1,30 @@
+using UnityEditor;
+using YG;
+
+public class WarlabZYG
+{
+    [MenuItem("WarLabZ/Reset Progress")]
+    public static void ResetProgress()
+    {
+        YG2.saves.levelStars = new int[] { -1, -1, -1, -1 };
+        YG2.saves.cash = 0;
+        YG2.saves.donateCash = 0;
+        YG2.saves.unlockedWeapons = new int[] { 0 };
+        YG2.saves.supplies = new int[] { 0, 0, 0 };
+        YG2.saves.points = 0;
+        YG2.saves.level = 0;
+        YG2.saves.achievements = new int[] { 0 };
+        YG2.saves.keys = 0;
+        YG2.saves.wallLevel = 9;
+        YG2.saves.weaponLevels = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        YG2.saves.selectedWeapon = 0;
+        YG2.SaveProgress();
+    }
+
+    [MenuItem("WarLabZ/Add 100 cash")]
+    public static void Add100Cash()
+    {
+        YG2.saves.cash = YG2.saves.cash + 100;
+        YG2.SaveProgress();
+    }
+}
