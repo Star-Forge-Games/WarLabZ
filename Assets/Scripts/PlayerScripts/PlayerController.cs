aab1068f5e6daf1572ddc354d1e3c28ee9c3ebaf
Ivan <carefall.regular@gmail.com>
2025-06-26 17:08:57 +0300

    Update PlayerController.cs

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -84,9 +84,10 @@ public class PlayerController : MonoBehaviour
 
     public void TouchStart(InputAction.CallbackContext context)
     {
-        if (!context.started || !context.canceled) return;
-        Debug.Log("TouchStart, reset " + context.phase);
-        touchPosition = Vector2.zero;
+        if (context.started || context.canceled) {
+            Debug.Log("TouchStart, reset " + context.phase);
+            touchPosition = Vector2.zero;
+        }
     }
 
 
