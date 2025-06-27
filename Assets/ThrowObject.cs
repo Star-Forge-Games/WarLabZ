8ea9a90959d1c3d725ff9a90d37a4e1f2ed58202
Ivan <carefall.regular@gmail.com>
2025-06-27 15:04:22 +0300

    Update ThrowObject.cs

--- Assets/ThrowObject.cs
+++ Assets/ThrowObject.cs
@@ -5,6 +5,7 @@ public class ThrowObject : MonoBehaviour
 
     private int damage;
     private EnemyZombie z;
+    [SerializeField] GameObject boomprefab;
 
     public void Setup(EnemyZombie z, int damage)
     {
@@ -16,6 +17,10 @@ public class ThrowObject : MonoBehaviour
     {
         Wall.instance.TakeDamage(z, damage);
         Destroy(gameObject);
+        Vector3 pos = this.transform.position;
+        pos.z = 1.5f;
+        pos.y = 1.5f;
+        Instantiate(boomprefab, pos, Quaternion.identity);
     }
 
 }
