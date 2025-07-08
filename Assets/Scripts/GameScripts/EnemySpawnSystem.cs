f1b1d3bcaedc03598693edf742ce748775fd222e
Ivan <carefall.regular@gmail.com>
2025-07-08 11:32:08 +0300

    Update EnemySpawnSystem.cs

--- Assets/Scripts/GameScripts/EnemySpawnSystem.cs
+++ Assets/Scripts/GameScripts/EnemySpawnSystem.cs
@@ -24,8 +24,8 @@ public class EnemySpawnSystem : MonoBehaviour
     private int endlessWave = 0;
     private bool lastZombieSpawned = false;
     private bool paused = false;
-    private int wave = 0;
-    private int spawnedEnemies = 0, waveEnemies = 0;
+    [SerializeField] private int wave = 0;
+    [SerializeField] private int spawnedEnemies = 0, waveEnemies = 0;
     private Action<bool> action;
     private float endlessTimer;
     private bool endlessStarted;
